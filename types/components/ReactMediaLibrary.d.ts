8f0d3753d0a4dfc52cf306ed1dbb1089609d9f7d
Richard <richardhung111@gmail.com>
2019-09-29 10:23:05 -0700

    File library props

--- types/components/ReactMediaLibrary.d.ts
+++ types/components/ReactMediaLibrary.d.ts
@@ -1,7 +1,8 @@
 import * as React from 'react';
 import {FileUploadProps} from "./FileUpload";
+import {FileLibraryProps} from "./FileLibrary";
 
-export interface ReactMediaLibraryProps extends FileUploadProps {
+export interface ReactMediaLibraryProps extends FileUploadProps, FileLibraryProps {
 	show:boolean;
 	onHide: () => void;
 	modalTitle?: string;
