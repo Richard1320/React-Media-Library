107bee87d6a8a368dcfce7391cf02b97433fada3
Richard <richardhung111@gmail.com>
2019-10-03 22:44:30 -0700

    Check if file library list is empty

--- src/components/FileLibrary.tsx
+++ src/components/FileLibrary.tsx
@@ -6,6 +6,8 @@ import Row from "react-bootstrap/Row";
 
 const FileLibrary: React.FC<FileLibraryProps> = (props: FileLibraryProps): JSX.Element => {
 	function renderList(): ReactNode[] {
+		if (!props.fileLibraryList) return [];
+
 		const render: ReactNode[] = [];
 		props.fileLibraryList.forEach((element: FileLibraryListItem, index: number) => {
 			render.push(<Col key={index} xs={12} sm={6} md={4} lg={3} className="mb-3">
