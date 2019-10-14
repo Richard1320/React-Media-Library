46f5cb6d1ffd4b3a6e85ee3f1dbe0c4606d2b52d
Richard <richardhung111@gmail.com>
2019-10-13 21:03:33 -0700

    Add missing interface to main file

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,6 +1,6 @@
 export {ReactMediaLibrary, ReactMediaLibraryProps} from "./components/ReactMediaLibrary";
 export {ReactMediaLibraryTabsProps} from "./components/ReactMediaLibraryTabs";
-export {FileUploadProps, FileMeta} from "./components/FileUpload";
+export {FileUploadProps, FileUploadListItem, FileMeta} from "./components/FileUpload";
 export {FileUploadListProps} from "./components/FileUploadList";
 export {FileLibraryProps, FileLibraryListItem} from "./components/FileLibrary";
 export {FileLibraryPagerProps} from "./components/FileLibraryPager";
